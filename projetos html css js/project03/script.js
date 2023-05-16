b608b7c2361de6d377d2b65171866fd4f1677abf
João Vitor <joaovitorlopesarruda@gmail.com>
2023-05-16 11:41:20 -0300

    Revert "=("
    This reverts commit 0a6aea10fa95068013801b3b38445dacfe9d80f4.

--- projetos html css js/project03/script.js
+++
@@ -1,7 +0,0 @@
-function verificar(){
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.getElementById('res')
-    if(fano)
-}
